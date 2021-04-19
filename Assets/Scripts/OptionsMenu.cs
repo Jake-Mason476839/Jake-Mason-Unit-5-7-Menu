ae7612d780c554c58af6210efe38d7d0d7af3259
Jake-Mason476839 <72437199+Jake-Mason476839@users.noreply.github.com>
2021-04-19 14:02:32 +0100

    (Rework) changes
    changes to music and options

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -9,6 +9,36 @@ public class OptionsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
 
+    public Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+
+    void start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+            resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolution = i;
+            }
+        }
+
+        resolutionDropdown.Addoptions();
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
     public void SetVolume (float volume)
     {
         audioMixer.SetFloat("MusicVolume", volume);
@@ -27,5 +57,11 @@ public class OptionsMenu : MonoBehaviour
     public void SetQuality (int qualityIndex)
     {
         QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetResolution (int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 }
